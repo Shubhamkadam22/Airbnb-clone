5e5c740563f539c850f7eda84882b3ef5dbf4e34
Shubham Kadam <shubhamkadam22100@gmail.com>
2026-08-02 16:54:51 +0530

    DELETE route created

--- app.js
+++ app.js
@@ -1,18 +1,27 @@
-const express = require('express');
+const express = require('express');                                  //import express module
 
-const app = express();  
-const listing = require('./model/listing.js');
-const mongoose = require('mongoose');
-const MONGO_URL = 'mongodb://127.0.0.1:27017/Wonderlust';
-const Listing = require("./model/listing.js");
-const path = require('path');
+const app = express();                                              //create an instance of express
+const listing = require('./model/listing.js');                      //import the listing model
+const mongoose = require('mongoose');                               //import mongoose module
+const MONGO_URL = 'mongodb://127.0.0.1:27017/Wonderlust';           //set the MongoDB connection URL
+const Listing = require("./model/listing.js");                     //import the listing model
+const path = require('path');                                      //import path module
+
+//import method-override module to support PUT and DELETE methods in forms
 
 const methodOverride = require('method-override');
 app.use(methodOverride('_method'));
 
+
+// set up the view engine and views directory
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
+
+//middleware to parse incoming request bodies
 app.use(express.urlencoded({ extended: true }));
+
+
+//database connection
 
 async function main(){
     await mongoose.connect(MONGO_URL);
@@ -21,6 +30,7 @@ async function main(){
 main().then(() => console.log('Connected to MongoDB'))
     .catch(err => console.error('Failed to connect to MongoDB', err));
 
+ //port to run the server
 
 app.listen(3000, () => {
     console.log('Server is running on port 3000');
@@ -28,17 +38,20 @@ app.listen(3000, () => {
 });
 
 //main page route
+
 app.get('/', (req, res) => {
     res.send('hello root');
 });
 
 //Index route 
+
 app.get("/listings", async (req, res) => {
   const allListings = await listing.find({});
   res.render("listings/index.ejs", { listings: allListings });
 });
 
 //New Route 
+
 app.get("/listings/new" , (req , res) => {
  res.render("listings/new.ejs");
 
@@ -46,6 +59,7 @@ app.get("/listings/new" , (req , res) => {
 
 
 //Show route 
+
 app.get("/listings/:id", async (req, res) => {
     let { id } = req.params;
     const listing = await Listing.findById(id);
@@ -62,6 +76,7 @@ res.redirect("/listings");
 });
 
 
+
 //Edit route 
 
 app.get("/listings/:id/edit", async (req, res) => {
@@ -71,14 +86,20 @@ app.get("/listings/:id/edit", async (req, res) => {
 });
 
 //Update route
+
 app.put("/listings/:id", async (req, res) => {
     let { id } = req.params;
     const updatedListing = await Listing.findByIdAndUpdate(id, req.body.listing, { new: true });
     res.redirect(`/listings/${id}`);
 });
 
+//Delete route 
 
-
+app.delete("/listings/:id", async (req, res) => {
+    let { id } = req.params;
+    let deletedListing = await Listing.findByIdAndDelete(id);
+    res.redirect("/listings");
+}); 
 
 
 
